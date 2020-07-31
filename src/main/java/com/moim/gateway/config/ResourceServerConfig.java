89ee81a2d898154bd2791a66a956c5c7bef217c8
cdssw <cdssw@naver.com>
2020-07-31 20:58:33 +0900

    회원가입용 api 허용

--- src/main/java/com/moim/gateway/config/ResourceServerConfig.java
+++ src/main/java/com/moim/gateway/config/ResourceServerConfig.java
@@ -39,6 +39,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers(WHITE_LIST).permitAll() // white list만 허용
 			.antMatchers(HttpMethod.POST, "/user/signup/").permitAll() // 회원가입 허용
+			.antMatchers(HttpMethod.GET, "/user/check/**").permitAll() // 회원가입용 체크는 허용
 			.anyRequest().authenticated(); // 그 외 요청은 access_token이 있어야 가능
 	}
 }
